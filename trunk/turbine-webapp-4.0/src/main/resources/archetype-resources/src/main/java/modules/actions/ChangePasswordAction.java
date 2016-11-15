febb2ec1071f1164f9c70da0603c01b939dbd0db
gk <gk@13f79535-47bb-0310-9956-ffa450edef68>
2016-11-15 10:29:45 +0000

    - some bugfixes in pom, sql, .. - set explicit order in sql execute - add examples: action password change, login to check for anonymous user login - update README.txt
    git-svn-id: https://svn.apache.org/repos/asf/turbine/maven/archetypes@1769768 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/turbine-webapp-4.0/src/main/resources/archetype-resources/src/main/java/modules/actions/ChangePasswordAction.java
+++ trunk/turbine-webapp-4.0/src/main/resources/archetype-resources/src/main/java/modules/actions/ChangePasswordAction.java
@@ -19,55 +19,28 @@ package ${package}.modules.actions;
 * under the License.
 *#
 
-import org.apache.fulcrum.security.model.turbine.TurbineAccessControlList;
-import org.apache.turbine.modules.actions.VelocitySecureAction;
+
+import org.apache.fulcrum.security.util.PasswordMismatchException;
+import org.apache.turbine.annotation.TurbineService;
 import org.apache.turbine.om.security.User;
 import org.apache.turbine.pipeline.PipelineData;
+import org.apache.turbine.services.security.SecurityService;
+import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 
 /**
- * Velocity Secure action.
+ * Change Password action.
  *
- * Always performs a Security Check that you've defined before executing the
- * doPerform().
  */
-public class SecureAction extends VelocitySecureAction 
+public class ChangePasswordAction extends SecureAction 
+
 {
-	/**
-	 * This currently only checks to make sure that user is allowed to view the
-	 * storage area. If you create an action that requires more security then
-	 * override this method.
-	 *
-	 * @param data
-	 *            Turbine information.
-	 * @return True if the user is authorized to access the screen.
-	 * @exception Exception,
-	 *                a generic exception.
-	 */
-	@Override
-	protected boolean isAuthorized(PipelineData data) throws Exception 
-	{
-		boolean isAuthorized = false;
-
-		// Who is our current user?
-		User user = getRunData(data).getUser();
-
-		// Get the Turbine ACL implementation
-		TurbineAccessControlList acl = getRunData(data).getACL();
-
-		if (acl == null || ! ( acl.hasRole("turbineuser") || acl.hasRole("turbineadmin") ) ) 
-		{
-			getRunData(data).setMessage("You do not have permission to access this action");
-			isAuthorized = false;
-		} 
-		else if ( acl.hasRole("turbineuser") || acl.hasRole("turbineadmin") )
-		{
-			isAuthorized = true;
-		}
-
-		return isAuthorized;
-	}
-
+	
+    /** Injected service instance */
+    @TurbineService
+    private SecurityService security;
+	
+	
 	/**
 	 * Implement this to add information to the context.
 	 *
@@ -78,8 +51,45 @@ public class SecureAction extends VelocitySecureAction
 	 * @exception Exception,
 	 *                a generic exception.
 	 */
+    @Override
+    public void doPerform(PipelineData data)
+            throws Exception 
+            {
+
+		User user = getRunData(data).getUser();
+		
+    RunData rundata = getRunData(data);
+    String oldPassword = rundata.getParameters().getString("oldpassword", "");
+    String newPassword = rundata.getParameters().getString("newpassword", "");
+		
+    try {
+        security.changePassword(user, oldPassword, newPassword); 
+        rundata.setMessage("Password changed!");
+    }
+    catch (PasswordMismatchException e) 
+    {
+      rundata.setMessage(e.getMessage());
+      rundata.setScreenTemplate("Password.vm");
+    }
+		
+    
+	}
+    
+	/**
+	 * Implement this to add information to the context.
+	 *
+	 * @param data
+	 *            Turbine information.
+	 * @param context
+	 *            Context for web pages.
+	 * @exception Exception,
+	 *                a generic exception.
+	 */
+	@Override
 	public void doPerform(PipelineData data, Context context) throws Exception 
 	{
 
+		context.put("success", "Password changed!!");
 	}
+
 }
