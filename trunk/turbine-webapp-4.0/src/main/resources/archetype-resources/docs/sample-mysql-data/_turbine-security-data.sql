e00096cd38c0f2ddac1921ed1aaae4015626db26
painter <painter@13f79535-47bb-0310-9956-ffa450edef68>
2017-12-14 15:19:16 +0000

    update SQL statements to upper case to conform with MySQL case sensitivity on Linux platforms
    git-svn-id: https://svn.apache.org/repos/asf/turbine/maven/archetypes@1818144 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/turbine-webapp-4.0/src/main/resources/archetype-resources/docs/sample-mysql-data/_turbine-security-data.sql
+++ trunk/turbine-webapp-4.0/src/main/resources/archetype-resources/docs/sample-mysql-data/_turbine-security-data.sql
@@ -16,23 +16,23 @@ INSERT INTO TURBINE_USER (USER_ID,LOGIN_NAME,PASSWORD_VALUE,FIRST_NAME,LAST_NAME
 INSERT INTO TURBINE_USER (USER_ID,LOGIN_NAME,PASSWORD_VALUE,FIRST_NAME,LAST_NAME)
     VALUES (3,'anon','nopw','','Anon');
 
-INSERT INTO `turbine_permission` (`PERMISSION_ID`, `PERMISSION_NAME`) VALUES
+INSERT INTO TURBINE_PERMISSION (`PERMISSION_ID`, `PERMISSION_NAME`) VALUES
 (2, 'Turbine'),
 (1, 'TurbineAdmin');
 
-INSERT INTO `turbine_role` (`ROLE_ID`, `ROLE_NAME`) VALUES
+INSERT INTO TURBINE_ROLE (`ROLE_ID`, `ROLE_NAME`) VALUES
 (1, 'turbineadmin'),
 (2, 'turbineuser');
 
-INSERT INTO `turbine_group` (`GROUP_ID`, `GROUP_NAME`) VALUES
+INSERT INTO TURBINE_GROUP (`GROUP_ID`, `GROUP_NAME`) VALUES
 (1, 'global'),
 (2, 'Turbine');
 
-INSERT INTO `turbine_role_permission` (`ROLE_ID`, `PERMISSION_ID`) VALUES
+INSERT INTO TURBINE_ROLE_PERMISSION (`ROLE_ID`, `PERMISSION_ID`) VALUES
 (1, 1),
 (2, 2);
 
-INSERT INTO `turbine_user_group_role` (`USER_ID`, `GROUP_ID`, `ROLE_ID`) VALUES
+INSERT INTO TURBINE_USER_GROUP_ROLE (`USER_ID`, `GROUP_ID`, `ROLE_ID`) VALUES
 (1, 1, 1),
 (1, 2, 1),
 (2, 2, 2),
@@ -40,12 +40,12 @@ INSERT INTO `turbine_user_group_role` (`USER_ID`, `GROUP_ID`, `ROLE_ID`) VALUES
 
 -- 
 ALTER TABLE TURBINE_USER MODIFY COLUMN USER_ID INT auto_increment;
-ALTER TABLE turbine_permission MODIFY COLUMN PERMISSION_ID INT auto_increment;
-ALTER TABLE turbine_role MODIFY COLUMN ROLE_ID INT auto_increment;
-ALTER TABLE turbine_group MODIFY COLUMN GROUP_ID INT auto_increment;
+ALTER TABLE TURBINE_PERMISSION MODIFY COLUMN PERMISSION_ID INT auto_increment;
+ALTER TABLE TURBINE_ROLE MODIFY COLUMN ROLE_ID INT auto_increment;
+ALTER TABLE TURBINE_GROUP MODIFY COLUMN GROUP_ID INT auto_increment;
 
 
-##-- use ID_TABLE  witk idMethod="idbroker" and set appropriate start_id and end_id 
+##-- use ID_TABLE  with idMethod="idbroker" and set appropriate start_id and end_id 
 ##-- INSERT INTO ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1,'TURBINE_USER',start_id, end_id);
     
 
